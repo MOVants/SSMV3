5112c541307e05f3ca444d8349a622767e872aaa
marvin <marvin@Gnome>
2018-10-24 13:41:44 +0800

    Added External Jars

--- SSMWebV3/src/DriversModule.java
+++ SSMWebV3/src/DriversModule.java
@@ -12,28 +12,30 @@ import org.openqa.selenium.*;
 public class DriversModule extends StringValues{
 	private WebDriver driver;
 	private String baseUrl;
-	
-	
-	  @Before
-	  public void setUp() throws Exception {
-//		  System.setProperty("webdriver.gecko.driver","/home/marvin/Documents/Selinium/geckodriver"); 
-//	    driver = new FirefoxDriver();
-		  System.setProperty("webdriver.chrome.driver", "/home/marvin/Documents/Selinium/chromedriver");
-		  driver = new ChromeDriver();
-		  baseUrl = "http://172.16.0.133:8000";
-		  driver.manage().window().maximize();
-		  driver.get(baseUrl + "/satellite/user/login");
-		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
-	  
-	  @Test
-	  public void loginTest() throws Exception {
+
+
+	@Before
+	public void setUp() throws Exception {
+
+
+//		System.setProperty("webdriver.gecko.driver","/home/marvin/Documents/Selinium/geckodriver"); 
+//		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "/home/marvin/Documents/Selinium/chromedriver");
+		driver = new ChromeDriver();
+		baseUrl = "http://172.16.0.133:8000";
+		driver.manage().window().maximize();
+		driver.get(baseUrl + "/satellite/user/login");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void loginTest() throws Exception {
 		loginUser();
 		System.out.println("Login - Test Pass");
-	  } 
-	
+	} 
+
 	@Test
-	  public void createDriverTest() throws Exception {
+	public void createDriverTest() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -50,10 +52,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingNameTest() throws Exception {
+	public void createDriverMissingNameTest() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -69,10 +71,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Name- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingMidNameTest() throws Exception {
+	public void createDriverMissingMidNameTest() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -88,10 +90,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Middle Name- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingLastNameTest() throws Exception {
+	public void createDriverMissingLastNameTest() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -107,10 +109,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Last Name- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingcontactNumberTest() throws Exception {
+	public void createDriverMissingcontactNumberTest() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -126,11 +128,11 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Contact Number- Test Failed");
 		}
-	  }
-	
-	
+	}
+
+
 	@Test
-	  public void createDriverMissingEmailTest() throws Exception {
+	public void createDriverMissingEmailTest() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -146,10 +148,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Email- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverGenderIsMale() throws Exception {
+	public void createDriverGenderIsMale() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -166,11 +168,11 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Gender is Male- Test Failed");
 		}
-	  }
-	
-	
+	}
+
+
 	@Test
-	  public void createDriverMissingLicenseNum() throws Exception {
+	public void createDriverMissingLicenseNum() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -186,10 +188,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing license Number- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingAddress() throws Exception {
+	public void createDriverMissingAddress() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -205,10 +207,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Address- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingLicenseExpiryDate() throws Exception {
+	public void createDriverMissingLicenseExpiryDate() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -224,10 +226,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing License Expiry Date- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void createDriverMissingDateOfBirth() throws Exception {
+	public void createDriverMissingDateOfBirth() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -243,10 +245,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Create Driver Missing Date Of Birth- Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void cancelCreateDriver() throws Exception {
+	public void cancelCreateDriver() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -256,8 +258,8 @@ public class DriversModule extends StringValues{
 		Thread.sleep(1000);
 		cancelDriver();
 		for (String handle : driver.getWindowHandles()) {
-		    driver.switchTo().window(handle);
-		    }
+			driver.switchTo().window(handle);
+		}
 		Thread.sleep(3000);
 		if (driver.getPageSource().contains("NAVIGATION"))
 		{
@@ -265,11 +267,11 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Cancel Create Driver - Test Failed");
 		}
-	  }
-	
-	
+	}
+
+
 	@Test
-	  public void modifyDriverDetails() throws Exception {
+	public void modifyDriverDetails() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -288,10 +290,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Details - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverName() throws Exception {
+	public void modifyDriverName() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -311,10 +313,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Name - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverMidName() throws Exception {
+	public void modifyDriverMidName() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -334,10 +336,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Middle Name - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverLastName() throws Exception {
+	public void modifyDriverLastName() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -357,11 +359,11 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Last Name - Test Failed");
 		}
-	  }
-	
-	
+	}
+
+
 	@Test
-	  public void modifyDriverContNum() throws Exception {
+	public void modifyDriverContNum() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -381,10 +383,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Contact Number - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverEmail() throws Exception {
+	public void modifyDriverEmail() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -404,10 +406,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Email - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverGender() throws Exception {
+	public void modifyDriverGender() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -427,10 +429,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Email - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverLicenseNum() throws Exception {
+	public void modifyDriverLicenseNum() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -450,10 +452,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver License Number - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverDateOfBirth() throws Exception {
+	public void modifyDriverDateOfBirth() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -473,10 +475,10 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Modify Driver Date of Birth - Test Failed");
 		}
-	  }
-	
+	}
+
 	@Test
-	  public void modifyDriverCancel() throws Exception {
+	public void modifyDriverCancel() throws Exception {
 		loginUser();
 		driverManagement();
 		Thread.sleep(1000);
@@ -491,29 +493,29 @@ public class DriversModule extends StringValues{
 		}else {
 			System.out.println("Cancel Modification of Driver Details - Test Failed");
 		}
-	  }
+	}
 
 
 
 	private void loginUser() {
-		  driver.findElement(By.xpath(btnAuthLogin)).click();
-		  driver.findElement(By.id(usernameField)).sendKeys(authusername);
-		  driver.findElement(By.id(passwordField)).sendKeys(authpass);
-		  driver.findElement(By.xpath(btnLogin)).click();
+		driver.findElement(By.xpath(btnAuthLogin)).click();
+		driver.findElement(By.id(usernameField)).sendKeys(authusername);
+		driver.findElement(By.id(passwordField)).sendKeys(authpass);
+		driver.findElement(By.xpath(btnLogin)).click();
 	}
-	
+
 	private void driverManagement() {
 		driver.findElement(By.xpath(driverManagementBtn)).click();
 	}
-	
+
 	private void clickCreateDriverBtn() {
 		driver.findElement(By.xpath(createDriverBtn)).click();
 	}
-	
+
 	private void editDriverBtn() {
 		driver.findElement(By.xpath(editBtn)).click();
 	}
-	
+
 	private void setDriverDetails() {
 		driver.findElement(By.id(firstNameField)).sendKeys(RandomStringUtils.randomNumeric(6));
 		driver.findElement(By.id(middleNameField)).sendKeys(RandomStringUtils.randomNumeric(6));
@@ -526,20 +528,24 @@ public class DriversModule extends StringValues{
 		driver.findElement(By.id(addressField)).sendKeys(RandomStringUtils.randomNumeric(6));
 		driver.findElement(By.id(licenseExpiryDateField)).sendKeys("02022019");
 		driver.findElement(By.id(dateOfBirthField)).sendKeys("02021992");
-			
+
 	}
-	
+
 	private void saveDriver() {
 		driver.findElement(By.xpath(savedDriverBtn)).click();
 	}
-	
+
 	private void cancelDriver() {
 		driver.findElement(By.xpath(cancelDriverBtn));
 	}
 
+	private void setBrowserConfig() {
+
+	}
+
 	@After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	  }
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
 }
 
